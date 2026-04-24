29d16bba5ec77a2fcfdf199f6dce5ea1311019d1
kevVaquerano <145158804+kevVaquerano@users.noreply.github.com>
2026-04-23 22:16:34 -0600

    done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\StoreController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Registro
+Route::post('/register', [RegisterController::class, 'register']);
+
+// Tienda y Carrito
+Route::get('/catalog', [StoreController::class, 'catalog']);
+Route::post('/cart/add/{id}', [StoreController::class, 'addToCart']);
+Route::post('/cart/checkout', [StoreController::class, 'checkout'])->middleware('auth');
