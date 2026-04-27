5fffc64d9d410acbf2d6480169d394f32a7015bd
Daniel Iván López García <lopez.garcia1319@gmail.com>
2026-04-26 22:34:43 -0600

    ✨ creando modelos para factura y cliente

---
+++ app/Models/Cliente.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Table;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable(['id_cliente', 'user_id', 'nombres', 'apellidos', 'dui', 'fecha_nacimiento'])]
+#[Table('clientes')]
+#[Table(key: 'id_cliente')]
+class Cliente extends Model{
+ 
+    public function facturas()
+    {
+        return $this->hasMany(Factura::class, 'id_cliente', 'id_cliente');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
---
+++ app/Models/Factura.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Table;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable(['id_factura', 'id_cliente', 'fecha_compra', 'total_pagado', 'metodo_pago'])]
+#[Table('facturas')]
+#[Table(key: 'id_factura')]
+class Factura extends Model{
+
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class, 'id_cliente', 'id_cliente');
+    }
+
+    public function cuponesComprados()
+    {
+        return $this->hasMany(CuponComprado::class, 'id_factura', 'id_factura');
+    } 
+}
